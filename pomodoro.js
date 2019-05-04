88562e64784e4f68ab0b87923e98a8ea69128c9d
Ben Daniels <benbanj@hotmail.com>
2019-05-04 14:25:21 +0100

    Formatting time display

--- pomodoro.js
+++ pomodoro.js
@@ -1,13 +1,29 @@
 let display = document.querySelector('#timer-display');
+let minDisplay = document.querySelector('#mins');
+let secDisplay = document.querySelector('#secs');
 let startButton = document.querySelector('#play-btn');
 let pauseButton = document.querySelector('#pause-btn');
 let stopButton = document.querySelector('#stop-btn');
+let time = 300;
 let timerId;
 
+function displayTime() {
+    let mins = Math.floor(time / 60);
+    let secs = time % 60;
+    if (mins < 10) mins = convertSingleDigit(mins);
+    if (secs < 10) secs = convertSingleDigit(secs);
+    minDisplay.textContent = mins;
+    secDisplay.textContent = secs;
+}
+displayTime();
+
+function convertSingleDigit(num) {
+    return "0" + num;
+}
+
 function countDown() {
-    let time = display.textContent;
-    time--
-    display.textContent = time;
+    displayTime();
+    time--;
 }
 
 function start() {
@@ -23,7 +39,8 @@ pause();
 function stop() {
     stopButton.addEventListener('click', function() {
         clearInterval(timerId);
-        display.textContent = 25;
+        time = 300;
+        displayTime();
     });
 }
 stop();
