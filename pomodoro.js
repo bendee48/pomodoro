0fddce0c0c5d8bd38bcf7d29d7a14704df39fe9b
Ben Daniels <benbanj@hotmail.com>
2019-05-04 10:32:44 +0100

    Adding main start, stop, pause logic

--- pomodoro.js
+++ pomodoro.js
@@ -1,1 +1,38 @@
-console.log("Test Test");
+let display = document.querySelector('#timer-display');
+let startButton = document.querySelector('#play-btn');
+let pauseButton = document.querySelector('#pause-btn');
+let stopButton = document.querySelector('#stop-btn');
+let timerId;
+
+function countDown() {
+    let time = display.textContent;
+    time--
+    display.textContent = time;
+}
+
+function start() {
+    startButton.addEventListener('click', startTimer);
+}
+start();
+
+function pause() {
+    pauseButton.addEventListener('click', function() {
+        console.log("pause");
+        clearInterval(timerId);
+    });
+}
+pause();
+
+function stop() {
+    stopButton.addEventListener('click', function() {
+        console.log("stop");
+        clearInterval(timerId);
+        display.textContent = 25;
+    });
+}
+stop();
+
+function startTimer() {
+    timerId = setInterval(countDown, 1000);
+}
+
