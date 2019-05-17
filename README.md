00c2fe529bf37ed76ac92fe7f901ce200a8bf842
Ben Daniels <benbanj@hotmail.com>
2019-05-17 14:08:04 +0100

    Style page

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 The Odin Project
 
-Project: Pair Programming
+Project: Pair Programming/ Pomodoro clock
 
 Task
 - Create a Pomodoro clock.
+- Work with a partner.
 - Clock will countdown 25 mins for work, 5 mins for a break, then start over again.
+- Implement pause/ stop/ restart button.
+- Implement ability to set work/ break time.
--- pomodoro.js
+++ pomodoro.js
@@ -13,8 +13,8 @@ const breakIncrease = document.querySelector('#break-up');
 const breakDecrease = document.querySelector('#break-down');
 const audio = new Audio('beep-08b.mp3');
 
-let breakLength = 60;
-let time = 60;
+let breakLength = 300;
+let time = 1500;
 let timerId;
 let onBreak = false;
 let timeRunning = false;
@@ -22,120 +22,120 @@ let currentTime;
 let currentBreakTime;
 
 function displayTime() {
-    let mins = Math.floor(time / 60);
-    let secs = time % 60;
-    if (mins < 10) mins = convertSingleDigit(mins);
-    if (secs < 10) secs = convertSingleDigit(secs);
-    minDisplay.textContent = mins;
-    secDisplay.textContent = secs;
+  let mins = Math.floor(time / 60);
+  let secs = time % 60;
+  if (mins < 10) mins = convertSingleDigit(mins);
+  if (secs < 10) secs = convertSingleDigit(secs);
+  minDisplay.textContent = mins;
+  secDisplay.textContent = secs;
 }
 displayTime();
 
-function setUpDisplays() { 
-    sessionTime.textContent = Math.floor(time / 60);
-    breakDisplay.textContent = Math.floor(breakLength / 60)
+function setUpDisplays() {
+  sessionTime.textContent = Math.floor(time / 60);
+  breakDisplay.textContent = Math.floor(breakLength / 60)
 }
 setUpDisplays();
 
 function increaseSessionTime() {
-    sessionUp.addEventListener('click', () => {
-        if (timeRunning) return;
-        time += 60;
-        displayTime();
-        sessionTime.textContent = Math.floor(time / 60)
-    });
+  sessionUp.addEventListener('click', () => {
+    if (timeRunning) return;
+    time += 60;
+    displayTime();
+    sessionTime.textContent = Math.floor(time / 60)
+  });
 }
 increaseSessionTime();
 
 function decreaseSessionTime() {
-    sessionDown.addEventListener('click', () => {
-        if (timeRunning || time <= 61) return;
-        time -= 60;
-        displayTime();
-        sessionTime.textContent = Math.floor(time / 60)
-    });
+  sessionDown.addEventListener('click', () => {
+    if (timeRunning || time <= 61) return;
+    time -= 60;
+    displayTime();
+    sessionTime.textContent = Math.floor(time / 60)
+  });
 }
 decreaseSessionTime();
 
 function increaseBreakTime() {
-    breakIncrease.addEventListener('click', () => {
-        if (timeRunning) return;
-        breakLength += 60;
-        breakDisplay.textContent = Math.floor(breakLength / 60);
-    });
+  breakIncrease.addEventListener('click', () => {
+    if (timeRunning) return;
+    breakLength += 60;
+    breakDisplay.textContent = Math.floor(breakLength / 60);
+  });
 }
 increaseBreakTime();
 
 function decreaseBreakTime() {
-    breakDecrease.addEventListener('click', () => {
-        if (timeRunning  || breakLength <= 61) return;
-        breakLength -= 60;
-        breakDisplay.textContent = Math.floor(breakLength / 60);
-    });
+  breakDecrease.addEventListener('click', () => {
+    if (timeRunning || breakLength <= 61) return;
+    breakLength -= 60;
+    breakDisplay.textContent = Math.floor(breakLength / 60);
+  });
 }
 decreaseBreakTime();
 
 function convertSingleDigit(num) {
-    return "0" + num;
+  return "0" + num;
 }
 
 function countDown() {
-    timeRunning = true;
-    time--;
-    displayTime();
-    if (time <= 5) audio.play();
-    if (time === 0 && onBreak === false) breakTime();
-    if (time === 0 && onBreak === true) {
-        time = currentTime;
-        onBreak = false;
-        statusText();
-    }
+  timeRunning = true;
+  time--;
+  displayTime();
+  if (time <= 5) audio.play();
+  if (time === 0 && onBreak === false) breakTime();
+  if (time === 0 && onBreak === true) {
+    time = currentTime;
+    onBreak = false;
+    statusText();
+  }
 }
 
 function breakTime() {
-    time = currentBreak;
-    onBreak = true;
-    statusText();
+  time = currentBreak;
+  onBreak = true;
+  statusText();
 }
 
 function start() {
-    startButton.addEventListener('click', () => {
-        currentTime = sessionTime.textContent * 60;
-        currentBreak = breakDisplay.textContent * 60;
-        startTimer();
-    });
+  startButton.addEventListener('click', () => {
+    currentTime = sessionTime.textContent * 60;
+    currentBreak = breakDisplay.textContent * 60;
+    startTimer();
+  });
 }
 start();
 
 function pause() {
-    pauseButton.addEventListener('click', () => {
-        clearInterval(timerId);
-        timeRunning = false;
-    });
+  pauseButton.addEventListener('click', () => {
+    clearInterval(timerId);
+    timeRunning = false;
+  });
 }
 pause();
 
 function stop() {
-    stopButton.addEventListener('click', function() {
-        clearInterval(timerId);
-        timeRunning = false;
-        time = sessionTime.textContent * 60;
-        displayTime();
-        status.textContent = "Work, Work, Work, Work, Work";
-    });
+  stopButton.addEventListener('click', function () {
+    clearInterval(timerId);
+    timeRunning = false;
+    time = sessionTime.textContent * 60;
+    displayTime();
+    status.textContent = "Let's get to work!";
+  });
 }
 stop();
 
 function startTimer() {
-    if (timeRunning) return;
-    timerId = setInterval(countDown, 1000);
+  if (timeRunning) return;
+  timerId = setInterval(countDown, 1000);
 }
 
 function statusText() {
-    if (onBreak) {
-        status.textContent = "On a break."
-    } else {
-        status.textContent = "Back at it!"
-    }
+  if (onBreak) {
+    status.textContent = "On a break."
+  } else {
+    status.textContent = "Back at it!"
+  }
 }
 
